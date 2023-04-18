b81a9624877dd5cae1a3a4bc5d90823e17ec324d
Crowdbotics <engineering@crowdbotics.com>
2023-04-18 12:46:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-darkness-dev-77309",
+  url: "https://square-darkness-dev-77309.botics.co",
+  api: "https://square-darkness-dev-77309.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
